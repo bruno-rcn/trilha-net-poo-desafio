6758b75b96755211a804faa5b0af68078733450b
BrunoNoberto <bruno.norberto@konicaminolta.pt>
2024-05-04 18:27:56 +0100

    desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("1234567890", "3310", "11111", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Maps");
+
+Iphone iphone = new Iphone("0987654321", "IphoneX", "22222", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Waze");
